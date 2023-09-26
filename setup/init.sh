4d38b6066d4f3e75ab0f84ea8c2e98364e6c7889
cuong8121999 <50694314+cuong8121999@users.noreply.github.com>
2023-09-26 23:44:19 +0700

    changed latest version to 18

--- setup/init.sh
+++ setup/init.sh
@@ -2,15 +2,14 @@
 set -e -o pipefail
 
 echo "Fetching IAM github-action-user ARN"
-userarn=$(aws iam get-user --user-name github-action-user | jq -r .User.Arn)
+userarn=$"(aws iam get-user --user-name github-action-user | jq -r .User.Arn)"
 
 # Download tool for manipulating aws-auth
 echo "Downloading tool..."
-curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_linux_amd64 -o aws-iam-authenticator
-chmod +x aws-iam-authenticator
+brew install aws-iam-authenticator
 
 echo "Updating permissions"
-./aws-iam-authenticator add user --userarn="${userarn}" --username=github-action-role --groups=system:masters --kubeconfig="$HOME"/.kube/config --prompt=false
+aws-iam-authenticator add user --userarn="arn:aws:iam::245310611731:user/github-action-user" --username=github-action-user --groups=system:masters --kubeconfig="$HOME"/.kube/config --prompt=false
 
 echo "Cleaning up"
 rm aws-iam-authenticator
